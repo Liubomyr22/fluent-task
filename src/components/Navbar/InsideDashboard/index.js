2e18b1ab3f255aa21dba40f168813fe695d27b85
mike-kovtun <66732810+mike-kovtun@users.noreply.github.com>
2021-03-15 17:51:50 +0200

    before stylesation

--- src/components/Navbar/InsideDashboard/index.js
+++ src/components/Navbar/InsideDashboard/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import { Flex, Image, Text, Header } from '@fluentui/react-northstar'
+import InsideDashBoardContent from '../InsideDashBordContent';
 
 const InsideDashboard = () => (
-  <Flex gap="gap.medium"  debug>
-    <Flex.Item grow>
-      <Flex column gap="gap.small" vAlign="stretch">
-        <Flex space="between">
-          <Header as="h3" content="LOREM IPSUM" />
-          <Text as="pre" content="Oct 24th, 00:01" />
-        </Flex>
-        <Text content="Manblog." />
-        <Flex.Item push>
-          <Text as="pre" content="COPYRIGHT: Fluent UI." />
-        </Flex.Item>
-      </Flex>
-    </Flex.Item>
+    <Flex debug>
+   <InsideDashBoardContent />
+   <InsideDashBoardContent />
   </Flex>
 )
 
